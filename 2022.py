9c58c9e27130693942c025da5c7a93db4229c199
KangSmail01 <71451617+KangSmail01@users.noreply.github.com>
2021-11-27 10:19:53 +0700

    Update 2022.py

--- 2022.py
+++ 2022.py
@@ -27,7 +27,7 @@ logo = ("""\x1b[1;92m ___ ___ ___ __  __ ___ _   _ __  __
 \x1b[1;96m|_| |_|_\___|_|  |_|___|\___/|_|  |_|
 \x1b[1;96m[\x1b[1;97m#\x1b[1;96m]\x1b[1;97m--------------------------
 \x1b[1;95m[\x1b[1;97m-->\x1b[1;95m]\x1b[1;97m au : KangSmail
-\x1b[1;95m[\x1b[1;97m-->\x1b[1;95m]\x1b[1;97m fb : fb.com/rozhak.xyz
+\x1b[1;95m[\x1b[1;97m-->\x1b[1;95m]\x1b[1;97m yt : https://youtu.be/loF5goP0Co4
 \x1b[1;95m[\x1b[1;97m-->\x1b[1;95m]\x1b[1;97m gh : github.com/KangSmail01
 \x1b[1;96m[\x1b[1;97m#\x1b[1;96m]\x1b[1;97m--------------------------
 """)
@@ -65,7 +65,7 @@ def login():
 	elif login == "2":
 		print("\x1b[1;96m[\x1b[1;97m*\x1b[1;96m]\x1b[1;97m Anda Akan Diarahkan Ke Browser")
 		time.sleep(3)
-		os.system("xdg-open https://youtu.be/3Y6xsMB3wRg")
+		os.system("xdg-open https://youtu.be/loF5goP0Co4")
 		exit()
 	elif login == "0":
 		exit()
